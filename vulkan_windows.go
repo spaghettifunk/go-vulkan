a625d64c61d5d8925a069e6fe4d5d968bb97cc25
Brent Farris <1002223+BrentFarris@users.noreply.github.com>
2024-01-29 11:28:44 -0600

    Fixing forked build

--- vulkan_windows.go
+++ vulkan_windows.go
@@ -3,6 +3,15 @@
 
 package vulkan
 
+/*
+#cgo CFLAGS: -DVK_USE_PLATFORM_WIN32_KHR
+
+#include "vk_wrapper.h"
+#include "vk_bridge.h"
+#include "vulkan/vulkan_win32.h"
+*/
+import "C"
+
 import "unsafe"
 
 func Win32SurfaceCreateInfoKHRHelper(hwnd, hInstance unsafe.Pointer, instance Instance, surface *Surface) Result {
